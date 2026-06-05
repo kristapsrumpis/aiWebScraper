516c38e99d41b1f5798b76629e76da6b4457534a
Kristaps Rumpis <kristapsrumpis.kr@gmail.com>
2026-06-05 15:39:22 +0100

    add promt line in ai request

--- src/controlers/ai_request.js
+++ src/controlers/ai_request.js
@@ -1,21 +1,35 @@
 // const {} = require("./../config");
 const { scrapePage } = require("./../services/scraperService");
 const { callAI } = require("./../services/aiService");
-const { AI_AGENT_INSTRUCTIONS } = require("./../config");
+const {
+  AI_AGENT_INSTRUCTIONS,
+  AI_MEMORY_FILE,
+  AI_MEMORY_DIR,
+} = require("./../config");
 const {
   loadMemory,
   updateMemory,
   saveMemory,
 } = require("./../services/memoryService");
+const path = require("path");
 
 async function ai_request(req, res) {
-  const data = req.body;
-  let page;
+  const userInput = req.body;
   let aiResponse;
+  const memory = path.join(AI_MEMORY_DIR, AI_MEMORY_FILE);
+  const prompt = `
+    SYSTEM INS|TRUCTIONS:
+    ${JSON.stringify(AI_AGENT_INSTRUCTIONS, null, 2)}
+
+    MEMORY:
+    ${JSON.stringify(memory, null, 2)}
+
+    USER REQUEST:
+    ${JSON.stringify(userInput)}
+    `;
 
   try {
-    page = await scrapePage(data.userInputURI);
-    aiResponse = await callAI(data.userInputPrompt);
+    aiResponse = await callAI(prompt);
   } catch (err) {
     return {
       success: false,
